d99edaf289b4dbe34867aa11942c9eb4217a7228
Yashism <yashvwaghmare2002@gmail.com>
2022-11-20 22:51:09 -0600

    Walmart and Target WebScraping Working

--- app/src/main/java/com/akshaj02/shopnsplit/productSearch.java
+++ app/src/main/java/com/akshaj02/shopnsplit/productSearch.java
@@ -2,27 +2,29 @@ package com.akshaj02.shopnsplit;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Instant;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
+
 import android.os.AsyncTask;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -31,6 +33,8 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 
 public class productSearch extends Activity {
@@ -50,8 +54,6 @@ public class productSearch extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_search);
-
-        Log.d(TAG, "**** APP START ****");
         context = getApplicationContext();
 
         // GUI init
@@ -65,9 +67,6 @@ public class productSearch extends Activity {
             public void onClick(View v) {
 
                 final String searchString = mEditText.getText().toString();
-                String txt = "Searching for : " + searchString;
-                Log.d(TAG, txt);
-                mTextView.setText(txt);
 
                 // hide keyboard
                 InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -131,6 +130,7 @@ public class productSearch extends Activity {
 
 
             try {
+//                assert conn != null;
                 responseCode = conn.getResponseCode();
                 responseMessage = conn.getResponseMessage();
             } catch (IOException e) {
@@ -145,41 +145,173 @@ public class productSearch extends Activity {
                 if(responseCode != null && responseCode == 200) {
 
                     // response OK
-
                     BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
                     StringBuilder sb = new StringBuilder();
-                    String line;
-                    String[][] productDetails = new String[5][5];
 
+                    //make json from response
+                    String line = "";
                     while ((line = rd.readLine()) != null) {
-                        sb.append(line + "\n");
+                        sb.append(line);
                     }
                     rd.close();
 
-                    conn.disconnect();
+                    //parse json to get titles and names for each item
+                    String json = sb.toString();
+                    JSONObject obj = new JSONObject(json);
+                    JSONArray items = obj.getJSONArray("items");
 
-                    result = sb.toString();
+                    String[] links = new String[items.length()];
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
 
-                    JSONObject resultJson = new JSONObject(result);
-                    JSONArray items = resultJson.getJSONArray("items");
+                        links[i] = item.getString("link");
+                    }
+                    //now we have the titles and links for each item
+                    //check if link has "walmart" in it
+                    //if it does, then we have a walmart link
 
-                    for(int i = 0; i < 5; i++) {
-                        String title = items.getJSONObject(i).getString("title");
-                        String link = items.getJSONObject(i).getString("link");
-                        productDetails[i][0] = title;
-                        productDetails[i][1] = link;
+
+                    String walmartLink = "";
+                    for (String value : links) {
+                        if (value.contains("walmart")) {
+                            walmartLink = value;
+
+                            break;
+                        }
                     }
 
-//                    return items.toString(4);
-                    return productDetails[0][0] + "\nURL: " + productDetails[0][1] + "\n\n\n" + productDetails[1][0] + "\nURL: " + productDetails[1][1] + "\n\n\n" + productDetails[2][0] + "\nURL: " + productDetails[2][1] + "\n\n\n" + productDetails[3][0] + "\nURL: " + productDetails[3][1] + "\n\n\n" + productDetails[4][0] + "\nURL: " + productDetails[4][1] + "\n";
-                }else{
+                    //same for target
+                    String targetLink = "";
+                    for (String s : links) {
+                        if (s.contains("target")) {
+                            targetLink = s;
+                            break;
+                        }
+                    }
+
+                    //same for kroger
+                    String krogerLink = "";
+                    for (String link : links) {
+                        if (link.contains("kroger")) {
+                            krogerLink = link;
+                            break;
+                        }
+                    }
+
+                    conn.disconnect();
+
+                    //now we have the links for each store
+                    //we need to parse the html for each link to get the price and title
+                    //then we need to display the results
+
+                    //walmart
+                    Document doc = Jsoup.connect(walmartLink).get();
+                    //do the work only if connection is successful
+                    String walmartTitle = "";
+                    String walmartPrice = "0";
+                    float walmartPriceFloat = 0;
+                    if (doc != null)
+                    {
+                        //select html with custom css selector
+                        walmartTitle = doc.select("h1.f3.b.lh-copy.dark-gray.mt1.mb2").text();
+                        walmartPrice = doc.select("span.inline-flex.flex-column").text();
+                        //select only 1 of the 2 html elements with the same css selector
+                        walmartPrice = walmartPrice.substring(0, walmartPrice.indexOf(" "));
+                        //remove any non-numeric characters
+                        walmartPrice = walmartPrice.replaceAll("[^\\d.]", "");
+                        //convert to float
+                        walmartPriceFloat = Float.parseFloat(walmartPrice);
+                    }
+
+                    //target
+                    Document doc2 = Jsoup.connect(targetLink).get();
+                    String targetTitle = "";
+                    String targetPrice = "0";
+                    if (doc != null)
+                    {
+                        targetTitle = doc2.select("h1.Heading__StyledHeading-sc-1ihrzmk-0.jmSnUp.h-text-bold.h-margin-b-tight").text();
+                        URL url2 = new URL(targetLink);
+                        URLConnection connection = url2.openConnection();
+                        connection.connect();
+
+                        InputStream in = connection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        String line2;
+                        StringBuilder html = new StringBuilder();
+                        while ((line2 = reader.readLine()) != null) {
+                          html.append(line2);
+                        }
+                        reader.close();
+                        //Find the price
+                        int start = html.indexOf("formatted_current_price");
+                        int end = html.indexOf("formatted_current_price");
+                        while (html.charAt(end) != ',') {
+                            end++;
+                        }
+                        String priceTarget = html.substring(start, end);
+                        targetPrice = priceTarget.replaceAll("[^\\d.]", "");
+                    }
+
+//                    //kroger
+//                    Document doc3 = Jsoup.connect(krogerLink).get();
+//                    String krogerTitle = doc3.select("h1.ProductDetails-header.font-bold").text();
+//                    String krogerPrice = doc3.select("mark.kds-Price-promotional.kds-Price-promotional--plain.kds-Price-promotional--decorated").text();
+//                    //select 1st
+//                    krogerPrice = krogerPrice.substring(0, krogerPrice.indexOf(" "));
+//                    krogerPrice = krogerPrice.replaceAll("[^\\d.]", "");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    // return the links to be displayed
+                    return  "Walmart" + "\n" + walmartTitle + "\n" + walmartPriceFloat + "\n\n" + "Target" + "\n" + targetTitle + "\n" + targetPrice
+                            /*"Kroger" + "\n\n" + krogerTitle + "\n" + krogerPrice + "\n\n\n" + krogerLink*/  + "\n\nFUCK YES";
+                    //
+
+                }
+
+                else{
 
                     // response problem
 
-                    String errorMsg = "Http ERROR response " + responseMessage + "\n" + "Are you online ? " + "\n" + "Make sure to replace in code your own Google API key and Search Engine ID";
+                    String errorMsg = "Http ERROR response " + responseMessage;
                     Log.e(TAG, errorMsg);
                     result = errorMsg;
-                    return  result;
+                    return result;
 
                 }
             } catch (IOException e) {
@@ -206,9 +338,8 @@ public class productSearch extends Activity {
 
             // make TextView scrollable
             mTextView.setMovementMethod(new ScrollingMovementMethod());
-            // show result
+            //display the links
             mTextView.setText(result);
-
         }
 
 
